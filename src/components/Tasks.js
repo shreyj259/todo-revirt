97b4101d05694e8fdaa96c3d18ed4a638c82985f
shrey jain <shreyjain009@gmail.com>
2022-07-18 02:03:01 +0530

    added selector

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Task from './Task'
 
 const Tasks = ({taskList,handleDelete,handleComplete}) => {
+
+  const [selector,setSelector]=useState("true");
+
   return (
     <>
     <div className="subtext">
         Added task in to-do list
     </div>
+    <div className="selector-box">
+      <select onChange={(e)=>setSelector(e.target.value)}>
+      <option value="all">All</option>
+        <option value="true">Completed</option>
+        <option value="false">Pending</option>
+      </select>
+    </div>
     <div className="tasks-container">
-        {taskList && taskList.slice(0,50).map(item=><Task handleComplete={handleComplete} handleDelete={handleDelete} item={item}/>)}
+        {taskList && taskList.filter(item=>{
+          if(selector=="all")
+          return item;
+          else if(item.completed.toString()==selector)
+          return item;
+        }).slice(0,50).map(item=><Task handleComplete={handleComplete} handleDelete={handleDelete} item={item}/>)}
     </div>
     </>
   )
